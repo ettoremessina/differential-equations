6f00d63a7d88f3d9e70c4808d32adb01b54ca9b8
ettoremessina <et.messina@gmail.com>
2020-12-03 00:03:07 +0100

    Added tdeq ode_1st_ord_ivp_01.py

---
+++ ODEs/solver-demos/TorchDiffEq/ode_1st_ord_ivp_01.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+from torchdiffeq import odeint
+
+ode_fn1 = lambda t, x: torch.sin(t) + 3. * torch.cos(2. * t) - x
+
+an_sol = lambda t : (1./2.) * np.sin(t) - (1./2.) * np.cos(t) + (3./5.) * np.cos(2.*t) + (6./5.) * np.sin(2.*t) - (1./10.) * np.exp(-t)
+
+t_begin=0.
+t_end=10.
+t_nsamples=100
+t_space = np.linspace(t_begin, t_end, t_nsamples)
+x_init = torch.tensor([0.])
+
+x_an_sol = an_sol(t_space)
+
+x_num_sol = odeint(ode_fn1, x_init, torch.tensor(t_space))
+
+plt.figure()
+plt.plot(t_space, x_an_sol, label='analytical')
+plt.plot(t_space, x_num_sol, label='numerical')
+plt.title('ODE 1st order IVP solved by TorchDiffEq')
+plt.xlabel('t')
+plt.ylabel('x')
+plt.legend()
+plt.show()
+
