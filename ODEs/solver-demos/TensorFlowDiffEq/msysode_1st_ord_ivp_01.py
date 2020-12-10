863fecd524a24753358446cdc54fcb4d34cff7a0
ettoremessina <et.messina@gmail.com>
2020-12-10 23:43:27 +0100

    added sys of odes for tfdiffeq

---
+++ ODEs/solver-demos/TensorFlowDiffEq/msysode_1st_ord_ivp_01.py
@@ -0,0 +1,42 @@
+#Please see
+#https://computationalmindset.com/en/neural-networks/ordinary-differential-equation-solvers.html#sys1
+#for details
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tfdiffeq import odeint
+
+A = tf.constant([[-1., 1.],
+                 [4., -1.]],
+                 dtype=tf.float64)
+
+def ode_fn(t, X):
+	return A @ X
+
+an_sol_x = lambda t : np.exp(t) + np.exp(-3. * t)
+an_sol_y = lambda t : 2. * np.exp(t) - 2. * np.exp(-3. * t)
+
+t_begin=0.
+t_end=5.
+t_nsamples=100
+t_space = np.linspace(t_begin, t_end, t_nsamples)
+x_init = tf.constant([2.])
+y_init = tf.constant([0.])
+
+x_an_sol = an_sol_x(t_space)
+y_an_sol = an_sol_y(t_space)
+
+x_num_sol = odeint(ode_fn, tf.convert_to_tensor([x_init, y_init], dtype=tf.float64), tf.constant(t_space)).numpy()
+
+plt.figure()
+plt.plot(t_space, x_an_sol, label='analytical x')
+plt.plot(t_space, y_an_sol, label='analytical y')
+plt.plot(t_space, x_num_sol[:,0], label='numerical x')
+plt.plot(t_space, x_num_sol[:,1], label='numerical y')
+plt.title('System of two ODEs 1st order IVP solved by TensorFlowDiffEq')
+plt.xlabel('t')
+plt.legend()
+plt.show()
+
--- ODEs/solver-demos/TensorFlowDiffEq/ode_1st_ord_ivp_01.py
+++ ODEs/solver-demos/TensorFlowDiffEq/ode_1st_ord_ivp_01.py
@@ -1,10 +1,14 @@
+#Please see
+#https://computationalmindset.com/en/neural-networks/ordinary-differential-equation-solvers.html#ode1
+#for details
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
 from tfdiffeq import odeint
 
-ode_fn1 = lambda t, x: tf.math.sin(t) + 3. * tf.math.cos(2. * t) - x
+ode_fn = lambda t, x: tf.math.sin(t) + 3. * tf.math.cos(2. * t) - x
 
 an_sol = lambda t : (1./2.) * np.sin(t) - (1./2.) * np.cos(t) + \
                     (3./5.) * np.cos(2.*t) + (6./5.) * np.sin(2.*t) - \
@@ -18,7 +22,7 @@ x_init = tf.constant([0.])
 
 x_an_sol = an_sol(t_space)
 
-x_num_sol = odeint(ode_fn1, x_init, tf.constant(t_space))
+x_num_sol = odeint(ode_fn, x_init, tf.constant(t_space))
 
 plt.figure()
 plt.plot(t_space, x_an_sol, label='analytical')
