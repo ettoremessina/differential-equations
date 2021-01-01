02205f99d5920786fd779a0edc1bd71c2e8b104f
Ettore Messina <ettore.messina@kyklos-group.com>
2021-01-01 22:37:31 +0100

    renamed a folder name

--- ODEs/neural-odes-demos/julia/DiffEqFlux/fit-funcs-by-param-odes.jl
+++ ODEs/neural-odes-demos/julia/DiffEqFlux/fit-funcs-by-param-odes.jl
@@ -1,15 +1,30 @@
 #=
-x' = 2x + y + e^(-2t)
-y'= 4x-y - e^-(2t)
+For details, please visit:
+https://computationalmindset.com/en/neural-networks/experiments-with-neural-odes-in-julia.html#exp1
+
+Parametric system to train:
+x' = a1 x + b1 y + c1 e^(-d1 t)
+y' = a2 x + b2 y + c2 e^(-d2 t)
 x(0) = 0
 y(0) = 0
 
+to generate dataset use the following param values
+a1 = 1.11
+b1 = 2.43
+c1 = -3.66
+d1 = 1.37
+a2 = 2.89
+b2 = -1.97
+c2 = 4.58
+d2 = 2.86
+
+Analytical solution is:
 https://www.wolframalpha.com/input/?i=x%27+%3D+1.11+x+%2B+2.43+y+%2B+-3.66+e%5E%28-1.37+t%29%3B+y%27+%3D+2.89+x+-1.97+y+%2B+4.58+e%5E+%28-2.86+t%29%3B+x%280%29%3D0%3B+y%280%29%3D0%3B
 =#
 
 using Flux, DiffEqFlux, DifferentialEquations, Plots
 
-function parametric_ode_system(du,u,p,t)
+function parametric_ode_system!(du,u,p,t)
   x, y = u
   a1, b1, c1, d1, a2, b2, c2, d2 = p
   du[1] = dx = a1*x + b1*y + c1*exp(-d1*t)
@@ -41,9 +56,8 @@ trange = tbegin:tstep:tend
 u0 = [0.0,0.0]
 tspan = (tbegin,tend)
 p = ones(8)
-fparams = Flux.params(p)
 
-prob = ODEProblem(parametric_ode_system, u0, tspan, p)
+prob = ODEProblem(parametric_ode_system!, u0, tspan, p)
 
 function net()
     solve(prob, Tsit5(), p=p, saveat=trange)
@@ -63,9 +77,10 @@ opt = ADAM(learning_rate)
 callback_func = function ()
   println("loss: ", loss_func())
 end
+fparams = Flux.params(p)
 Flux.train!(loss_func, fparams, data, opt, cb=callback_func)
 
-predict_prob = ODEProblem(parametric_ode_system, u0, tspan, p)
+predict_prob = ODEProblem(parametric_ode_system!, u0, tspan, p)
 predict_sol = solve(prob, Tsit5(), saveat=trange)
 x_predict_sol = [u[1] for u in predict_sol.u]
 y_predict_sol = [u[2] for u in predict_sol.u]
@@ -74,7 +89,7 @@ println("Learned parameters:", p)
 
 plot(trange, dataset_outs[1],
     linewidth=2, ls=:dash,
-    title="Neural ODE to fit params",
+    title="Neural ODEs to fit params",
     xaxis="t",
     label="dataset x(t)",
     legend=true)
