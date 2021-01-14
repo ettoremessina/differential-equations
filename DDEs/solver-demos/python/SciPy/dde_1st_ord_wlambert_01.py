fixed a computation in dde solver with w lambert

DDEs/solver-demos/python/SciPy/dde_1st_ord_wlambert_01.py
@@ -57,14 +57,12 @@ def x(t):
         sk = SK[k + k_range]
         int_for_cki = integral_for_cki(sk)
         ck_denom = (1. + ad * h * np.exp(-sk * h))
-        cki = (x_0 + ad + np.exp(-sk * h) * int_for_cki) / ck_denom
+        cki = (x_0 + ad * np.exp(-sk * h) * int_for_cki) / ck_denom
         tot += np.exp(sk * t) * cki
     tot += integral_for_x_t()
     return tot
 
 x_num_sol=[x(t) for t in t_space]
-
-print(t_step)
 
 plt.figure()
 plt.plot(t_space, np.real(x_num_sol), linewidth=1, label='real')
@@ -77,12 +75,12 @@ plt.show()
 
 num_of_cells_for_h_time = int(h/t_step)
 
-x_num_grad_left = [np.real((x_num_sol[i+1] - x_num_sol[i])) / t_step 
+x_num_grad_left = [np.real((x_num_sol[i+1] - x_num_sol[i])) / t_step
 	for i in range(num_of_cells_for_h_time, t_nsamples-1)]
 x_num_grad_right = [
-	a * np.real(x_num_sol[i]) + 
-	ad * np.real(x_num_sol[i - num_of_cells_for_h_time]) 
-	+ b * u(t_space[i]) 
+	a * np.real(x_num_sol[i]) +
+	ad * np.real(x_num_sol[i - num_of_cells_for_h_time])
+	+ b * u(t_space[i])
 		for i in range(num_of_cells_for_h_time, t_nsamples-1)]
 
 plt.figure()
@@ -93,7 +91,3 @@ plt.xlabel('t')
 plt.ylabel('x(t)')
 plt.legend()
 plt.show()
-
-
-
-