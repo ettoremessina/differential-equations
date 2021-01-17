6c657b99b0fef5b9b2ef87afd885f429c4fca34e
Ettore Messina <ettore.messina@kyklos-group.com>
2021-01-17 23:20:38 +0100

    initial version of scripts for SymPy

---
+++ ODEs/solver-demos/python/SymPy/ode_1st_ord_linear_02.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sympy import *
+x, y, z, t = symbols('x y z t')
+k, m, n = symbols('k m n', integer=True)
+f, g, h = symbols('f g h', cls=Function)
+
+eq = Eq(f(t).diff(t), (t / (1 + t**2)) * f(t) + 1)
+an_sol = dsolve(eq, ics={f(0): 0})
+print('ODE class: ', classify_ode(eq)[0])
+pprint(an_sol)
+
+t_begin=0.
+t_end=10.
+t_nsamples=11
+t_space = np.linspace(t_begin, t_end, t_nsamples)
+
+lmbd_sol = lambdify(t, an_sol.rhs)
+x_an_sol = lmbd_sol(t_space)
+
+lmbd_handsol = lambda t : np.sqrt(1 + t**2) * np.log(t + np.sqrt(1 + t**2))
+x_hand_sol = lmbd_handsol(t_space)
+
+plt.figure()
+plt.plot(t_space, x_hand_sol, '--', linewidth=2, label='by hand')
+plt.plot(t_space, x_an_sol, linewidth=1, label='analytical')
+plt.title('linear variable ODE 1st order')
+plt.xlabel('t')
+plt.ylabel('f(t)')
+plt.legend()
+plt.show()
---
+++ ODEs/solver-demos/python/SymPy/ode_1st_ord_separable_01.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sympy import *
+x, y, z, t = symbols('x y z t')
+k, m, n = symbols('k m n', integer=True)
+f, g, h = symbols('f g h', cls=Function)
+
+eq = Eq(f(t).diff(t), -2 * t * f(t))
+an_sol = dsolve(eq, ics={f(0): 1})
+print(classify_ode(eq)[0])
+pprint(an_sol)
+
+t_begin=0.
+t_end=10.
+t_nsamples=101
+t_space = np.linspace(t_begin, t_end, t_nsamples)
+
+lmbd_sol = lambdify(t, an_sol.rhs)
+x_an_sol = lmbd_sol(t_space)
+
+plt.figure()
+plt.plot(t_space, x_an_sol, linewidth=1, label='analytical')
+plt.title('separable variable ODE 1st order')
+plt.xlabel('t')
+plt.ylabel('f(t)')
+plt.legend()
+plt.show()
