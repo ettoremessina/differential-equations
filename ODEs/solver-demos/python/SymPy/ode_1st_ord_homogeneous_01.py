0fb6a19bff2f2b2a76b49580d6f482d9e3d7097c
Ettore Messina <ettore.messina@kyklos-group.com>
2021-01-20 22:21:40 +0100

    added ode_1st_ord_homogeneous_01 and ode_1st_ord_separable_02

---
+++ ODEs/solver-demos/python/SymPy/ode_1st_ord_homogeneous_01.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sympy import *
+x, y, z, t = symbols('x y z t')
+k, m, n = symbols('k m n', integer=True)
+f, g, h = symbols('f g h', cls=Function)
+
+eq = eq = Eq(f(t).diff(t), (t**2 + f(t)**2)/(t * f(t)))
+an_sol = dsolve(eq, ics={f(2): 2}, hint='1st_homogeneous_coeff_best')
+print('ODE class: ', classify_ode(eq)[0])
+pprint(an_sol)
+
+t_begin=2.
+t_end=12.
+t_nsamples=101
+t_space = np.linspace(t_begin, t_end, t_nsamples)
+
+lmbd_sol = lambdify(t, an_sol.rhs)
+x_an_sol = lmbd_sol(t_space)
+
+plt.figure()
+plt.plot(t_space, x_an_sol, linewidth=1, label='analytical')
+plt.title('separable variable ODE 1st order')
+plt.xlabel('t')
+plt.ylabel('f(t)')
+plt.legend()
+plt.show()
