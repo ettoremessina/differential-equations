527ceedeef5fad1beb22ea9c6ebadb1ee7f14bc8
ettoremessina <et.messina@gmail.com>
2021-04-03 16:24:06 +0200

    minor change on example 03

--- ODEs/dyn-sys/python/nonlin-auton-plane-sys-hartman-analyzer-examples/example_03.sh
+++ ODEs/dyn-sys/python/nonlin-auton-plane-sys-hartman-analyzer-examples/example_03.sh
@@ -1,6 +1,6 @@
 python ../nonlin-auton-plane-sys-hartman-analyzer.py \
     --dx_dt "x * (1 - x/2. - y)" \
     --dy_dt "y * (x - 1 - y/2.)" \
-    --t_num_of_samples 500 \
+    --t_num_of_samples 400 \
     --x0_begin -3 --x0_end 3 \
     --y0_begin -3 --y0_end 3
